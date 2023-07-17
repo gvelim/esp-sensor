d81c4e2e72195d4bf03d98368f093684510f7dc4
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-07-17 22:11:02 +0100

    driving RGB LED with RMT

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use critical_section::Mutex;
 use core::cell::RefCell;
 
 use esp_backtrace as _;
-use esp_println::println;
+use esp_println::{println};
 use esp32c3::{
     clock::ClockControl, 
     peripherals::{Peripherals, Interrupt}, 
     prelude::*, 
     timer::TimerGroup, 
-    Rtc, IO, Delay, gpio::{Gpio9, Input, PullDown, Event}, interrupt
+    Rtc, IO, Delay, gpio::{Gpio9, Input, PullDown, Event}, interrupt, pulse_control::{ClockSource, PulseCode, RepeatMode}
 };
 
 static BUTTON: Mutex<RefCell<Option<Gpio9<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
@@ -58,10 +58,49 @@ fn main() -> ! {
         esp32c3::riscv::interrupt::enable();
     }
 
+    let mut rmt = esp32c3::PulseControl::new(
+        peripherals.RMT,
+        &mut system.peripheral_clock_control, 
+        ClockSource::APB, 
+        0, 
+        0, 
+        0).unwrap();
+
+    rmt.channel0
+        .set_idle_output_level(false)
+        .set_idle_output(true)
+        .set_carrier_modulation(false)
+        .set_channel_divider(1);
+
+    let mut ch0 = rmt.channel0.assign_pin(io.pins.gpio2);
+
+    // NanosDuration::<u32>::from_ticks((SK68XX_T0L_NS * (SOURCE_CLK_FREQ / 1_000_000)) / 500);
+    let t1 = PulseCode { level1: true, length1: 56u32.nanos(), level2: false, length2: 48u32.nanos() };
+    // let t1: u32 = (1u32 << 31) + (56u32 << 16) + (0u32 << 15) + 48u32;
+    let t0 = PulseCode { level1: true, length1: 28u32.nanos(), level2: false, length2: 64u32.nanos() };
+    // let t0: u32 = (1u32 << 31) + (28u32 << 16) + (0u32 << 15) + 64u32;
+    let rst = PulseCode { level1: false, length1: 2400u32.nanos(), level2: false, length2: 2400u32.nanos() };
+    // let rst = (1u32 << 31) + (2400u32 << 16) + (0u32 << 15) + 2400u32;
+    let eot = PulseCode { level1: false, length1: 0u32.nanos(), level2: false, length2: 0u32.nanos() };
+    // let eot = 0u32;
+
+    println!("TH {:b}, TL {:b}",u32::from(t1) as u32, u32::from(t0));
+    println!("TH {:X}, TL {:X}",u32::from(t1), u32::from(t0));
+    println!("TH {}, TL {}",u32::from(t1), u32::from(t0));
+
+    let mut rgb_data = [
+        t1, t0, t0, t0, t0, t0, t0, t0, // Green
+        t0, t0, t0, t0, t0, t0, t0, t0, // Red
+        t0, t0, t0, t0, t0, t0, t0, t0, // Blue
+        eot // end of transmission marker
+    ];
+
     let mut delay = Delay::new(&clocks);
     loop {
         led.toggle().unwrap();
-        delay.delay_ms(500u32);
+        ch0.send_pulse_sequence(RepeatMode::SingleShot, &rgb_data).unwrap();
+        rgb_data[0..24].rotate_left(1);
+        delay.delay_ms(200u32);
     }
 }
 
